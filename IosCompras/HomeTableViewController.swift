671725907085fd1c0fb071a1b21e72f9e036a784
André Lima <andre@reffer.com.br>
2017-05-30 00:12:43 -0300

    Comentários removidos

--- IosCompras/HomeTableViewController.swift
+++ IosCompras/HomeTableViewController.swift
@@ -14,27 +14,6 @@ import FBSDKLoginKit
 // https://bitbucket.org/danielvmacedo/demos_aulas_ios/src/90e8de873e7c8c7322f4f034950b5e93035877ed/13%20-%20FireBaseToDo/MyBase/GroupToDoTableViewController.swift?at=master&fileviewer=file-view-default
 
 
-/*
- Testar permissões no Firebase:
- 
- {
-   "rules": {
-     "Usuarios": {
-       "$idUsuario": {
-         ".read": "$idUsuario == auth.uid",
-         ".write": "$idUsuario == auth.uid"
-       }
-     },
-     "Listas": {
-       "$idLista": {
-         ".read": "root.child('Usuarios/' + auth.uid + '/MinhasListas/' + $idLista).exists()",
-         ".write": "root.child('Usuarios/' + auth.uid + '/MinhasListas/' + $idLista).exists()"
-       }
-     }
-   }
- }
- 
- */
 
 class HomeTableViewController: UITableViewController {
     
@@ -90,29 +69,11 @@ class HomeTableViewController: UITableViewController {
         self.UID = FIRAuth.auth()?.currentUser!.uid
         self.nameUser = FIRAuth.auth()?.currentUser!.displayName
         
-//        self.ref.child("Usuarios/" + self.UID! + "/MinhasListas" ).observeSingleEvent(of:.value, with: { (snapshot) in
-//            self.listaDeCompras.removeAll()
-//            for childSnapshot in snapshot.children {
-//                let child = childSnapshot as! FIRDataSnapshot
-//                let idLista = child.key as String
-//                
-//                // Pegar dados da lista:
-//                self.ref.child("Listas/" + idLista + "" ).observeSingleEvent(of: .value, with: { (snapshot) in
-//                    let value = snapshot.value as! [String: Any]
-//                    let newLista = Lista(title: value["title"] as? String, owner: value["owner"] as? String, itens: nil, ref:child.ref)
-//                    self.listaDeCompras.append(newLista)
-//                    self.tableView.reloadData()
-//                })
-//            }
-//        })
-        
-        // Alterar pasta para Usuarios/UID/MinhasListas
         self.ref.child("Usuarios/" + self.UID! + "/MinhasListas" ).observe(.childAdded, with: { (snapshot) in
             let idLista = snapshot.key as String
             
             print("Adicionada: " + idLista)
             
-            // TODO: Para cada ID de lista adicionado buscar os dados da lista em Listas/ID e criar o objeto
             // Pegar dados da lista:
             self.ref.child("Listas/" + idLista + "" ).observeSingleEvent(of: .value, with: { (snapshot) in
                 if (snapshot.value is NSNull) {
@@ -124,12 +85,7 @@ class HomeTableViewController: UITableViewController {
                 }
             })
             
-            // let indexPath = IndexPath(row: self.listaDeCompras.count - 1, section: 0)
-            //self.tableView.insertRows(at: [indexPath], with: .fade)
         })
-        
-        
-        // Alterar pasta para Usuarios/UID/MinhasListas
         
         self.ref.child("Usuarios/" + self.UID! + "/MinhasListas" ).observe(.childRemoved, with: { (snapshot) in
             let idLista = snapshot.key
@@ -166,15 +122,6 @@ class HomeTableViewController: UITableViewController {
             self.tableView.reloadData()
         })
         
-        /*
-         COLOCAR NA CLASSE DA LISTA DE ITENS
-        // Adicionar item
-        var objItem = ItemLista(name: "Novo Item", ref: nil)
-        self.ref.child("Listas").child(codLista).child("itens").childByAutoId().setValue(objItem.toAnyObject())
-        objItem = ItemLista(name: "Mais um Item", ref: nil)
-        self.ref.child("Listas").child(codLista).child("itens").childByAutoId().setValue(objItem.toAnyObject())
-         */
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -206,16 +153,6 @@ class HomeTableViewController: UITableViewController {
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -229,18 +166,6 @@ class HomeTableViewController: UITableViewController {
         }
     }
     
-    /**
-     Sent to the delegate when the button was used to login.
-     - Parameter loginButton: the sender
-     - Parameter result: The results of the login
-     - Parameter error: The error (if any) from the login
-     */
-
-    @IBAction func Logout(_ sender: UIBarButtonItem) {
-        logOut()
-        navigationController?.popViewController(animated: true)
-        print("logout")
-    }
     
     // Logout do professor: (atribuir a um botão da barra)
     func logOut(){
@@ -265,12 +190,6 @@ class HomeTableViewController: UITableViewController {
     // MARK: - Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-        
-        // TODO: Criar o segue.destinationViewController com cast para a classe de itens
-        // Atribuir o objeto lista ao objeto Lista dentro da classe para consultar itens do ID e título
-        
         
         if (segue.identifier == "LISTA_ITENS_SEGUE") {
             if let dvc = segue.destination as? ItensTableViewController {
--- IosCompras/ItensTableViewController.swift
+++ IosCompras/ItensTableViewController.swift
@@ -51,14 +51,6 @@ class ItensTableViewController: UITableViewController {
         return cell
     }
 
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
     /*
     // Override to support editing the table view.
@@ -72,29 +64,5 @@ class ItensTableViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
